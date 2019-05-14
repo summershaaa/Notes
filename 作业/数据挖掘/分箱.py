1c232884d59382e7413b18f88491236a85e97cb2
summershaaa <wenjx1024@qq.com>
2019-05-14 17:21:18 +0800

    Add files via upload

---
+++ 作业/数据挖掘/分箱.py
@@ -0,0 +1,162 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 14 14:25:26 2019
+
+@author: WinJX
+"""
+
+import numpy as np
+import copy
+class Box_Smoothing:
+    def __init__(self,data,depth,threshold):
+        self.data = data            #未分箱数据 
+        self.depth = depth          #箱子深度
+        self.threshold = threshold  #离群点阈值
+        
+    #该函数将原始数据进行分箱
+    def Bins(self):
+        length = len(self.data)  #数据的个数
+        #算出箱子的个数
+        if length%self.depth == 0:
+            bins = length//self.depth
+        else:
+            bins = length//self.depth + 1
+        #可能最后一个箱子数据不够，所以需要填充数据
+        diff = bins*self.depth - length
+        #填充若干个数据
+        for i in range(diff):
+            data.append(data[-1])
+            
+        #构造分箱后的数组,维度为：(箱数，箱深) 
+        array = np.array(self.data).reshape(bins,self.depth)
+        #返回分箱后的结果
+        return array
+    
+    #法一：按均值平滑，并返回离群点
+    def Smoothing_By_Mean(self):
+        #得到分箱后的数据
+        array = self.Bins()
+        print('--------分箱后未平滑的数据--------')
+        print(array)
+        #计算每行(每个箱子)的均值，并四舍五入
+        row_mean = np.round(array.mean(axis=1))
+        #进行均值平滑
+        for i in range(self.depth):
+            array[:,i] = row_mean
+            
+        print('--------按均值平滑法--------')
+        print(array)
+        
+    #法二：按中位数平滑
+    def Smoothing_By_Median(self):
+        #得到分箱后的数据
+        array = self.Bins()
+        #深拷贝原数组
+        copy_array = copy.deepcopy(array)
+        print('--------分箱后未平滑的数据--------')
+        print(array)
+        #得到每行(每个箱子)的中位数，并四舍五入
+        row_median = np.round(np.median(array,axis=1))
+        #进行中位数平滑
+        for i in range(self.depth):
+            array[:,i] = row_median
+        print('--------按中位数平滑法--------')
+        print(array)
+        #计算平滑后数组与原数组的绝对差值
+        diff = abs(array - copy_array)
+        #如果差值大于等于10，则为离群点
+        outlier = copy_array[diff>=self.threshold]
+        #存在离群点则打印出来
+        if outlier.size > 0:
+            print('离群点为:' ,outlier)
+            
+
+    #法三：按边界值平滑
+    def Smoothing_By_Boundary(self):
+        #得到分箱后的数据
+        array = self.Bins()
+        print('--------分箱后未平滑的数据--------')
+        print(array)
+        #左右边界值
+        left,right = array[:,0],array[:,-1]
+        #需要平滑的列数
+        diff = self.depth - 2
+        #进行按边界值平滑，平滑差值小的边界值，如果左右边界差值相等，默认选择左边界值
+        for col in range(1,diff+1):
+            t = array[:,col]          #取出当前列 
+            diff_left = abs(t-left)   #当前列与左边界的绝对差值
+            diff_right = abs(t-right) #当前列与右边界的绝对差值
+            #标记，如果当前列与左边界的差值 <= 当前列与右边界的差值，则标记为1，否则标记为0 
+            flag = np.where(diff_left <= diff_right,1,0)
+            #对当前列的每一个元素进行边界平滑
+            for idx in range(len(t)):
+                #如果当前索引下标记为1，则向左边界值平滑，否则向右边界值平滑
+                t[idx] = left[idx] if flag[idx] == 1 else right[idx]
+        #因为是原地操作，所以直接返回原数组即可
+        print('--------按边界值平滑法--------')
+        print(array)    
+
+if __name__ == '__main__':
+#data = [13, 15, 16, 16, 19, 20, 20, 21, 22, 22, 25, 25, 25, 25, 
+#        30, 33, 33, 35, 35, 35, 35, 36, 40, 45, 46, 52, 70]
+
+    data = np.loadtxt('./data.txt')
+    #为防止数据格式问题，统一把数据转化成列表形式
+    data = list(data)
+    print("请输入箱的深度")
+    depth = int(input())
+    threshold = 10   #设置离群点阈值
+    rst = Box_Smoothing(data,depth,threshold)
+    #按均值平滑，并输出离群点
+    rst.Smoothing_By_Mean()
+    print('--------------------------------')
+    #按中位数平滑
+    rst.Smoothing_By_Median()
+    print('--------------------------------')
+    #按边界值平滑
+    rst.Smoothing_By_Boundary()
+
+
+
+#length = len(data)
+#depth = 3  #箱深
+##算出箱子的个数
+#if length%depth == 0:
+#    bins = length//depth
+#else:
+#    bins = length//depth + 1
+##需要填充数据个数
+#diff = bins*depth - length
+#for i in range(diff):
+#    data.append(data[-1])
+##构造分箱后的数组,维度为：(箱数，箱深) 
+#array = np.array(data).reshape(bins,depth)
+##法一：按均值平滑
+#计算每行(每个箱子)的均值，并四舍五入
+#row_mean = np.round(array.mean(axis=1))
+#for i in range(depth):
+#    array[:,i] = row_mean
+#print(array)
+##法二：按中位数平滑
+##row_median = np.round(np.median(array,axis=1))
+##for i in range(depth):
+##    array[:,i] = row_median
+##print(array)
+#
+##法三：按边界值平滑
+##左右边界值
+#left,right = array[:,0],array[:,-1]
+#diff = depth - 2
+#for col in range(1,diff+1):
+#    t = array[:,col]          #取出当前列 
+#    diff_left = abs(t-left)   #当前列与左边界的绝对差值
+#    diff_right = abs(t-right) #当前列与右边界的绝对差值
+#    #标记，如果当前列与左边界的差值 <= 当前列与右边界的差值，则标记为1，否则标记为0 
+#    flag = np.where(diff_left <= diff_right,1,0)
+#    #对当前列的每一个元素进行边界平滑
+#    for idx in range(len(t)):
+#        #如果当前索引下标记为1，则向左边界值平滑，否则向右边界值平滑
+#        t[idx] = left[idx] if flag[idx] == 1 else right[idx]
+##因为是原地操作，所以直接返回原数组即可
+#    print('按边界值平滑法:')
+#print(array)  
